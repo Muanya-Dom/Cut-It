a9325a10a057b751821dc0b6a6fcef0cfc59b484
Muanya-Dom <chimuannyau@gmail.com>
2022-09-21 21:42:33 +0100

    Update

---
+++ CutIt/www/view/api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('SERVER_NAME','localhost');
+define('DB_USER','id15947990_cutit01');
+define('DB_PASS','qQ3]d\o_l\>\f1bl');
+define('DB_NAME','id15947990_cut_it');
+
+$cont=mysqli_connect(SERVER_NAME,DB_USER,DB_PASS,DB_NAME ) or die('db err');
+
+?>
